97bb0c38727454c90ae00cf0439b5c4f4c2fead9
saumyashukla <saumyashukla2909@gmail.com>
2026-04-27 22:24:18 +0530

    with dataBinding in mainActivity

--- app/src/main/java/com/example/myfirstxmlapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstxmlapp/MainActivity.kt
@@ -2,17 +2,37 @@ package com.example.myfirstxmlapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myfirstxmlapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nextButton.setOnClickListener {
+            val name = binding.nameInput.text.toString().trim()
+
+            if (name == "") {
+                binding.nameInput.error = "Please enter your name"
+            } else {
+                Toast.makeText(this, "Welcome $name", Toast.LENGTH_SHORT).show()
+
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("username", name)
+                startActivity(intent)
+            }
+        }
+
+        /*setContentView(R.layout.activity_main)
 
         val nameInput = findViewById<EditText>(R.id.nameInput)
         val button = findViewById<Button>(R.id.nextButton)
@@ -29,6 +49,6 @@ class MainActivity : AppCompatActivity() {
                 intent.putExtra("username", name)
                 startActivity(intent)
             }
-        }
+        }*/
     }
 }
